4842aea34ba34cd02cd9b9e89328c49ea1136433
V.H. Belvadi <vhbelvadi@VH-Belvadis-MacBook-Pro-13.local>
2021-05-30 09:46:00 +0530

    Cryptogram acceptor function

--- cryptogram_solver.py
+++ cryptogram_solver.py
@@ -20,7 +20,7 @@
 # 3. DUPLICATE THE CIPHER
 # 4. PARSE THE CIPHER AT SPACES OR PUNCTUATION INTO AN ARRAY
 # 5. ORDER ARRAY ELEMENTS BY LENGTH
-# 6. COMPARE ORIGINAL ARRAY AND ORDERED ARRY AND BUILD A DICTIONARY TO REMEMBER ORIGINAL ORDERING
+# 6. COMPARE ORIGINAL ARRAY AND ORDERED ARRAY AND BUILD A DICTIONARY TO REMEMBER ORIGINAL ORDERING
 # 7. DUPLICATE THE ARRAY (?)
 # 8. SUBSTITUTE THE CLUE INTO ALL ARRAY ELEMENTS
 # 9. COMPARE WITH DICTIONARY TO FIND WORD MATCHES IN ASCENSION OF ELEMENT LENGTH
@@ -41,14 +41,17 @@ import re  # RegEx to make sure the user entered a cryptogram
 
 def acceptor():
     cryptogram = input("Enter the cipher:\n")
-    if not re.search(r'^[a-zA-Z\s\D\.\,\'\"\?\!]+$', cryptogram):
+    if not re.search(r'^[a-zA-Z\s\D.,\'\"?!]+$', cryptogram):
         print('Invalid cryptogram. Please retry.\n\n')
         acceptor()
     else:
         return cryptogram
 
+
 def main():
     cipher = acceptor()
+    print(cipher)  # DELETE THIS LINE ONCE THE PROGRAM IS COMPLETE
+
 
 if __name__ == '__main__':
     main()
