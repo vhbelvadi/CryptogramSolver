a28e0e6e2e60485846273a11f8af92b71e8c1399
V.H. Belvadi <vhbelvadi@VH-Belvadis-MacBook-Pro-13.local>
2021-06-03 19:08:59 +0530

    Sorting and dictionary

--- sandbox.py
+++ sandbox.py
@@ -118,3 +118,33 @@
 # End of tkinter GUI test run
 ###############################################################################
 ###############################################################################
+
+original_list = ['g', 'fytc', 'y', 'bmg']
+length_sorted_list = []
+dictionary_memory = {}
+
+
+def sort_list_by_length():
+    temp_list = []
+    global original_list
+    for i in range(0, len(original_list)):
+        temp_list.insert(i, sorted(original_list, key=len)[i])
+    return temp_list
+
+
+def dictionary_builder(original, compare):
+    for original_element in original:
+        for compare_element in compare:
+            dictionary_memory[original_element] = compare_element
+            compare.remove(compare_element)
+            break
+
+
+if __name__ == '__main__':
+    print(original_list)
+    length_sorted_list = sort_list_by_length().copy()
+    print(length_sorted_list)
+    dictionary_builder(original_list, length_sorted_list)
+    # print(original_list)
+    # print(length_sorted_list)
+    print(str(dictionary_memory))
